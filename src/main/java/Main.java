087651a56bcf8f447705146f3408cb95e53e3d41
metalDude <yaroslav-tokar@yandex.ru>
2018-01-10 18:23:23 +0300

    Include IOExceptions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
+import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Main {
     public static final String url1 = "https://stackoverflow.com/questions/12470141/server-returned-http-response-code-400";
     public static final String url2 = "https://stackoverflow.com/questions/12470141/server-returned-http-response-code-400";
     public static final String url3 = "https://cwiki.apache.org//confluence/display/MAVEN/MojoFailureException";
-    public static final String url4 = "http://www.baeldung.com/java-8-parallel-streams-custom-threadpool";
+    public static final String url4 = "https://www.dns-shop.ru/";
+    public static final String regex = "(https?:\\/\\/)([\\w\\.]+)\\.([a-z]{2,6}\\.?)(\\/[\\w\\.]*)";
+    public static final String testGex = "<[Aa][\\s]{1}[^>]*[Hh][Rr][Ee][Ff][^=]*=[ '\\\"\\s]*([^ \\\"'>\\s#]+)[^>]*>";
+    public static final String easyRegex = "<a href=\"([^\"]+)";
 
-    public static void main(String[] args) {
+    public static final Pattern PATTERN = Pattern.compile(easyRegex);
 
-        HTMLOperation helper = HTMLHelper.getInstance();
-        helper.showAllLinksByUrl(url1);
-        helper.showAllLinksByUrl(url2);
-        helper.showAllLinksByUrl(url3);
-        helper.showAllLinksByUrl(url4);
 
+    public static void main(String[] args) throws IOException {
+
+/*
+        Set<String> hrefs = read();
+        Set<String> result = new HashSet<>();
+        Iterator<String> iterator = hrefs.iterator();
+        while (iterator.hasNext()) {
+            String temp = iterator.next();
+            //result.add(temp.replaceAll("<a href=\"", ""));
+            if (temp.contains("http") || temp.contains("https")) {
+
+                result.add(temp.replaceAll("<a href=\"", ""));
+            }
+        }
+        result.forEach(System.out::println);*/
+        HTMLHelper helper = HTMLHelper.getInstance();
+        Set<String> test1 = helper.getAllLinksByUrl(url4, easyRegex);
+        System.out.println("Test");
+        test1.forEach(System.out::println);
+        System.out.println("Test");
+        Set<String> test2 = helper.getAllLinksByUrl(url4, easyRegex);
+        test2.forEach(System.out::println);
+        System.out.println("Test");
+        Set<String> test3 = helper.getAllLinksByUrl(url4, easyRegex);
+        test3.forEach(System.out::println);
+
+    }
+
+    public static Set<String> read() throws IOException {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        File file = new File(loader.getResource("test").getFile());
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        Set<String> tempSet = new HashSet<>();
+        while (reader.ready()) {
+            tempSet.add(reader.readLine());
+        }
+        return tempSet;
+    }
+
+    public static String getClearUrl(String link) {
+
+        return new String();
     }
 }
 
 interface HTMLOperation {
-    Set<String> findUrlInHtml(String htmlPageContent, String regex);
+    Set<String> findUrlsInHtml(String htmlPageContent, String regex);
 
     String loadHtmlPageByUrl(String siteStringUrl);
 
@@ -36,19 +72,22 @@ interface HTMLOperation {
 
     Set<String> getAllLinksByUrl(String url, String pattern);
 
+    String cleanUrl(String badUrl);
+
 }
 
 class HTMLHelper implements HTMLOperation {
     //private String defaultPattern;
     private Set<String> linsk;
-
     private static HTMLHelper instance = new HTMLHelper();
     private String defaultRegex;
+    private String htmlContentPage;
 
 
     private HTMLHelper() {
         this.linsk = new HashSet<>();
         this.defaultRegex = "(https?:\\/\\/)([\\w\\.]+)\\.([a-z]{2,6}\\.?)(\\/[\\w\\.]*)";
+
     }
 
     public static synchronized HTMLHelper getInstance() {
@@ -77,10 +116,9 @@ class HTMLHelper implements HTMLOperation {
 
     @Override
     public Set<String> getAllLinksByUrl(String url, String pattern) {
-        StringBuilder builder = new StringBuilder();
         String htmlContent = loadHtmlPageByUrl(url);
         if (htmlContent != null) {
-            Set<String> urls = findUrlInHtml(htmlContent, pattern);
+            Set<String> urls = findUrlsInHtml(htmlContent, pattern);
             return urls;
         } else {
             return null;
@@ -88,17 +126,30 @@ class HTMLHelper implements HTMLOperation {
     }
 
     @Override
-    public Set<String> findUrlInHtml(String htmlPageContent, String regex) {
+    public String cleanUrl(String badUrl) {
+        if (badUrl.contains("http") || badUrl.contains("https")) {
+            return badUrl.replaceAll("<a href=\"", "");
+        }
+        return null;
+    }
+
+    @Override
+    public Set<String> findUrlsInHtml(String htmlPageContent, String regex) {
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(htmlPageContent);
         Set<String> temp = new HashSet<>();
         while (m.find()) {
-
-            temp.add(m.group());
-
+            if (m.group().contains("http") || m.group().contains("https")) {
+                temp.add(m.group().replaceAll("<a href=\"", ""));
+            }
         }
         return temp;
     }
+
+    public String getHtmlContentPage() {
+        return htmlContentPage;
+    }
+
 
     @Override
     public String loadHtmlPageByUrl(String siteStringUrl) {
@@ -118,6 +169,8 @@ class HTMLHelper implements HTMLOperation {
                             .append(buffer)
                             .append("\n");
                 }
+                this.htmlContentPage = resultHtml.toString();
+
                 return resultHtml.toString();
             }
         } catch (IOException e) {
